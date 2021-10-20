de686aada8450cdb6751fe76ccbd0b9b715e0211
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-10-20 09:57:47 -0400

    add movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,29 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.valid?
+      @movie.save
+      redirect_to movies_path(@movies)
+    else
+      render :new
+    end
+  end
+
+private
+  def movie_params
+    params.require(:movie).permit(:title, :subgenre, :summary, :poster_url)
+  end  
 end
