1d506692b1a7defd647c845bd536bc9a89a7ad7c
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-10-20 14:47:31 -0400

    installed devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_143155) do
+ActiveRecord::Schema.define(version: 2021_10_20_180816) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "summary"
-    t.string "subgenre"
-    t.string "poster_url"
+    t.string "genre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "poster_url"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -32,12 +32,13 @@ ActiveRecord::Schema.define(version: 2021_10_20_143155) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "image_url"
-    t.string "bio"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
