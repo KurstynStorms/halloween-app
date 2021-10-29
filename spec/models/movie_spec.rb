8002702cec4be3af880ce507b536abd7831cfa82
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-10-29 13:42:27 -0400

    added to movie model spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,6 +2,16 @@ require "rails_helper"
 
 RSpec.describe Movie, type: :model do
   #tests go here
+
+  describe "schema" do
+    it { is_expected.to have_db_column(:title).of_type(:string) }
+    it { is_expected.to have_db_column(:summary).of_type(:string) }
+    it { is_expected.to have_db_column(:genre).of_type(:string) }
+    it { is_expected.to have_db_column(:poster_url).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
   context 'movie validations' do
     it "movie is valid when all attributes are present" do
       expect(Movie.create(title: "YAY", summary: "dfsdfsf", genre: "sfgser", poster_url: "dfsdvd"))
